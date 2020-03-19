df2a9dfc7250cce8a2a723e51d16c412f35f0ed7
Buckoge <30780893+Buckoge@users.noreply.github.com>
2020-03-19 10:02:58 +0100

    first commit

---
+++ Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentAMovies.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string MovieDescription { get; set; }
+        public DateTime MovieDating { get; set; }
+
+    }
+
+}
